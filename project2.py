08924ab4a108a4064ff5b9ac8ff793cb0fd22dc2
Atul <arkshirsagar@mitaoe.ac.in>
2023-01-12 13:38:53 +0530

    Passed all columns of ticker data in csv

--- project2.py
+++ project2.py
@@ -25,7 +25,7 @@ def on_message(ws,message):
 file1 = open("market_data_level.csv","a")
 file1.write("Type,ProductId,Changes,,,Timestamp\n")
 def store_level2_data(data):
-    file1.write(data['type']+","+data['product_id']+","+str(data["changes"])+","+data["time"]+"\n")
+    file1.write(f"{data['type']},{data['product_id']},{data['changes']},{data['time']}\n")
     
 
 # wb = Workbook()
@@ -33,14 +33,14 @@ def store_level2_data(data):
 # ws.append(["timestamp", "price", "last_size", "bid", "ask"])
 # wb.save(filename = "market_data_ticker.xlsx")
 file2 = open("market_data_ticker.csv","a")
-file2.write("Timestamp, Price, Aast_size, Bid, ask\n")
+file2.write("Type,Sequence,Product_ID,Price,Open_24h,Volume_24h,Low_24h,High_24h,Volume_30d,Best_bid,Best_bid_size,Best_ask,Best_ask_size,Side,Trade_ID,Last_size,Time\n")
 def store_ticker_data(data):
     # ws = wb.active
     # ws.append([data["time"], data["price"], data["last_size"], data["best_bid"], data["best_ask"]])
     # wb.save(filename = "market_data_ticker.xlsx")
     # wb.close()
     print(data)
-    file2.write(data['time']+","+data["price"]+","+data["last_size"]+","+data["best_bid"]+","+data["best_ask"]+"\n")
+    file2.write(f"{data['type']},{data['sequence']},{data['product_id']},{data['price']},{data['open_24h']},{data['volume_24h']},{data['low_24h']},{data['high_24h']},{data['volume_30d']},{data['best_bid']},{data['best_bid_size']},{data['best_ask']},{data['best_ask_size']},{data['side']},{data['trade_id']},{data['last_size']},{data['time']}\n")
 
     
 socket = 'wss://ws-feed.pro.coinbase.com'
